f8b267b88f92c480125deee937fc279514b31f01
tommy <tommyho@cpp.edu>
2022-05-18 10:28:30 -0700

    setup CreateAppointment, need to resolve CORS on both sides loooool

--- src/components/Business.js
+++ src/components/Business.js
@@ -3,30 +3,18 @@ import React from "react";
 export default function Business(props) {
   /**
    *
-   * to view a business:
-   * if i click on a business from the landing page, it's coming from
-   * either /business?category={category} or /
-   * BUT i want the route to correspond to /business/{id} after clicking
-   *
    * to create an appointment:
    * i need a React component that is essentially a form
    * that has a couple of fields such as a textarea and a date
    * when the form is submitted, the form sends a POST request
-   * over to /appointments with the clientId, businessId, and date
-   * in the request body
-   * and additionally reroute the frontend URL
-   *
-   * BUT instead of using react-router to route the appointment...
-   * i don't actually care to expose the endpoint or not
-   * so therefore, i'll just use classic javascript
-   * to append the UI for appointments when clicked
-   *
+   * over to /appointments with the clientId, businessId, and date in the request body
+
    * BUT this kinda means that GetBusiness is useless
    * since it was to be used to build a business profile page
    */
 
   // TODO: use redux state to get jwt to fetch api
-  let jwt = window.jwt;
+  // let jwt = window.jwt;
 
   // TODO: ensure that images[0] is the profile picture
   // let profileURL = props.images
@@ -52,22 +40,52 @@ export default function Business(props) {
     });
   }
 
-  // TODO: when this business component is clicked, display the
-  // business profile UI (for now, just make it a form component)
-
   function createAppointment(event) {
     // TODO: POST /appointments and clear the form
     // TODO: validate required inputs
     event.preventDefault();
 
-    console.log(formData.comments);
-    console.log(formData.date);
-    console.log(formData.time);
+    // console.log(formData.comments);
+    // console.log(formData.date);
+    // console.log(formData.time);
 
     let rawDate = `${formData.date}T${formData.time}:00`;
     let dateObj = new Date(Date.parse(rawDate));
     let formattedDate = dateObj.toISOString();
-    console.log(formattedDate);
+
+    let fd = {
+      businessId: props.businessId,
+      // TODO: use the actual clientId
+      clientId: window.uid,
+      date: formattedDate,
+      // TODO: firebase implement "comments"
+      // fd.append("comments", formData.comments);
+    };
+    let fdJSON = JSON.stringify(fd);
+
+    let URL = `${process.env.REACT_APP_API_URL}appointments`;
+    // console.log(URL);
+
+    fetch(URL, {
+      method: "post",
+      body: fdJSON,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // FIXME: REMOVE BELOW IN PRODUCTION
+      mode: "no-cors",
+    })
+      .then((response) => {
+        console.log("status: " + response.status);
+        return response.text();
+      })
+      .then((data) => {
+        console.log(data);
+        alert(JSON.stringify(data));
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
   }
 
   return (
